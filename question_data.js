b046976c472542b13fef3e8b4a098242fcaac606
Steven Spiel <stevenspiel@gmail.com>
2013-12-20 10:50:11 -0600

    complete functionality of enabling/disabling buttons per question

--- question_data.js
+++ question_data.js
@@ -1,93 +1,93 @@
 window.Think = window.Think || {}
 window.Think.questions = [
   {answers: [ //Question 1
-    {name: 'Imaginative', category: 'C'},
-    {name: 'Investigative', category: 'D'},
-    {name: 'Realistic', category: 'A'},
-    {name: 'Analytical', category: 'B'}
+    {name: 'Imaginative', category: 'AR'},
+    {name: 'Investigative', category: 'CR'},
+    {name: 'Realistic', category: 'CS'},
+    {name: 'Analytical', category: 'AS'}
   ]},
   {answers: [ //Question 2
-    {name: 'Organized', category: 'A'},
-    {name: 'Adaptable', category: 'C'},
-    {name: 'Critical', category: 'B'},
-    {name: 'Inquisitve', category: 'D'}
+    {name: 'Organized', category: 'CS'},
+    {name: 'Adaptable', category: 'AR'},
+    {name: 'Critical', category: 'AS'},
+    {name: 'Inquisitve', category: 'CR'}
   ]},
   {answers: [ //Question 3
-    {name: 'Debating', category: 'B'},
-    {name: 'Getting to the Point', category: 'A'},
-    {name: 'Creating', category: 'D'},
-    {name: 'Relating', category: 'C'}
+    {name: 'Debating', category: 'AS'},
+    {name: 'Getting to the Point', category: 'CS'},
+    {name: 'Creating', category: 'CR'},
+    {name: 'Relating', category: 'AR'}
   ]},
   {answers: [ //Question 4
-    {name: 'Personal', category: 'C'},
-    {name: 'Practical', category: 'A'},
-    {name: 'Academic', category: 'B'},
-    {name: 'Adventurous', category: 'D'}
+    {name: 'Personal', category: 'AR'},
+    {name: 'Practical', category: 'CS'},
+    {name: 'Academic', category: 'AS'},
+    {name: 'Adventurous', category: 'CR'}
   ]},
   {answers: [ //Question 5
-    {name: 'Precise', category: 'A'},
-    {name: 'Flexible', category: 'C'},
-    {name: 'Systematic', category: 'B'},
-    {name: 'Inventive', category: 'D'}
+    {name: 'Precise', category: 'CS'},
+    {name: 'Flexible', category: 'AR'},
+    {name: 'Systematic', category: 'AS'},
+    {name: 'Inventive', category: 'CR'}
   ]},
   {answers: [ //Question 6
-    {name: 'Sharing', category: 'C'},
-    {name: 'Orderly', category: 'A'},
-    {name: 'Sensible', category: 'B'},
-    {name: 'Independent', category: 'D'}
+    {name: 'Sharing', category: 'AR'},
+    {name: 'Orderly', category: 'CS'},
+    {name: 'Sensible', category: 'AS'},
+    {name: 'Independent', category: 'CR'}
   ]},
   {answers: [ //Question 7
-    {name: 'Competitive', category: 'D'},
-    {name: 'Perfectionist', category: 'A'},
-    {name: 'Cooperative', category: 'C'},
-    {name: 'Logical', category: 'B'}
+    {name: 'Competitive', category: 'CR'},
+    {name: 'Perfectionist', category: 'CS'},
+    {name: 'Cooperative', category: 'AR'},
+    {name: 'Logical', category: 'AS'}
   ]},
   {answers: [ //Question 8
-    {name: 'Intellectual', category: 'B'},
-    {name: 'Sensitive', category: 'C'},
-    {name: 'Hardworking', category: 'A'},
-    {name: 'Risk-taking', category: 'D'}
+    {name: 'Intellectual', category: 'AS'},
+    {name: 'Sensitive', category: 'AR'},
+    {name: 'Hardworking', category: 'CS'},
+    {name: 'Risk-taking', category: 'CR'}
   ]},
   {answers: [ //Question 9
-    {name: 'Reader', category: 'B'},
-    {name: 'People person', category: 'C'},
-    {name: 'Problem Solver', category: 'D'},
-    {name: 'Planner', category: 'A'}
+    {name: 'Reader', category: 'AS'},
+    {name: 'People person', category: 'AR'},
+    {name: 'Problem Solver', category: 'CR'},
+    {name: 'Planner', category: 'CS'}
   ]},
   {answers: [ //Question 10
-    {name: 'Memorize', category: 'A'},
-    {name: 'Associate', category: 'C'},
-    {name: 'Think-through', category: 'B'},
-    {name: 'Originate', category: 'D'}
+    {name: 'Memorize', category: 'CS'},
+    {name: 'Associate', category: 'AR'},
+    {name: 'Think-through', category: 'AS'},
+    {name: 'Originate', category: 'CR'}
   ]},
   {answers: [ //Question 11
-    {name: 'Changer', category: 'D'},
-    {name: 'Judger', category: 'B'},
-    {name: 'Spontaneous', category: 'C'},
-    {name: 'Wants direction', category: 'A'}
+    {name: 'Changer', category: 'CR'},
+    {name: 'Judger', category: 'AS'},
+    {name: 'Spontaneous', category: 'AR'},
+    {name: 'Wants direction', category: 'CS'}
   ]},
   {answers: [ //Question 12
-    {name: 'Communicating', category: 'C'},
-    {name: 'Discovering', category: 'D'},
-    {name: 'Cautious', category: 'A'},
-    {name: 'Reasoning', category: 'B'}
+    {name: 'Communicating', category: 'AR'},
+    {name: 'Discovering', category: 'CR'},
+    {name: 'Cautious', category: 'CS'},
+    {name: 'Reasoning', category: 'AS'}
   ]},
   {answers: [ //Question 13
-    {name: 'Challenging', category: 'D'},
-    {name: 'Practicing', category: 'A'},
-    {name: 'Caring', category: 'C'},
-    {name: 'Examining', category: 'B'}
+    {name: 'Challenging', category: 'CR'},
+    {name: 'Practicing', category: 'CS'},
+    {name: 'Caring', category: 'AR'},
+    {name: 'Examining', category: 'AS'}
   ]},
   {answers: [ //Question 14
-    {name: 'Completing work', category: 'A'},
-    {name: 'Seeing possibilities', category: 'D'},
-    {name: 'Gaining ideas', category: 'B'},
-    {name: 'Interpreting', category: 'C'}
+    {name: 'Completing work', category: 'CS'},
+    {name: 'Seeing possibilities', category: 'CR'},
+    {name: 'Gaining ideas', category: 'AS'},
+    {name: 'Interpreting', category: 'AR'}
   ]},
   {answers: [ //Question 15
-    {name: 'Doing', category: 'A'},
-    {name: 'Feeling', category: 'C'},
-    {name: 'Thinking', category: 'B'},
-    {name: 'Experimenting', category: 'D'}
+    {name: 'Doing', category: 'CS'},
+    {name: 'Feeling', category: 'AR'},
+    {name: 'Thinking', category: 'AS'},
+    {name: 'Experimenting', category: 'CR'}
   ]}
 ];
--- think.js
+++ think.js
@@ -2,28 +2,141 @@ window.Think = window.Think || {}
 window.Think = _.extend(window.Think, {
     initialize: function() {
         this.currentQuestionNumber = 0;
-        this.optionCounter = 0;
-        this.setUpEvents();
+        // this.setUpEvents();
         this.showCurrentQuestion();
         this.listener();
         this.answersArray = [];
         this.answerSet = [];
         this.totals = {};
-        this.count = 0;
+        this.count = 0; 
+        this.optionCounter = 0; //change to length of currentlySelected
+        this.currentlySelected = [];
+        this.currentlyNotSelected = [this.currentQuestionNumber + "CS",this.currentQuestionNumber + "AS", this.currentQuestionNumber + "AR", this.currentQuestionNumber + "CR"];
+        this.goingBack = 0;
     },
-    setUpEvents: function() {},
+    // setUpEvents: function() {},
     showCurrentQuestion: function() {
         var question = this.currentQuestion();
         var buttonHtml = "";
         for (var i in question.answers) {
             var answer = question.answers[i];
-            buttonHtml = buttonHtml + "<input type='checkbox' class='individual-checkbox' value='" + answer.category + "'>" + answer.name + "<br>";
+            var id = this.currentQuestionNumber + answer.category
+            buttonHtml = buttonHtml + "<label for='" + id + "'><input type='checkbox' id='" + id + "'class='individual-checkbox' value='" + answer.category + "'>" + answer.name + "</label><br>";
         };
         $('.buttons').html(buttonHtml);
     },
     currentQuestion: function() {
         return this.questions[this.currentQuestionNumber];
     },
+    updateQuestionNumber: function(direction) {
+        if (direction === "forward") {
+            this.currentQuestionNumber += 1;
+        } else if (direction === "back") {
+            this.currentQuestionNumber -= 1;
+        }
+    },
+    reset: function(){
+        this.optionCounter = 0;
+        this.currentlySelected = [];
+        this.currentlyNotSelected = [this.currentQuestionNumber + "CS",this.currentQuestionNumber + "AS", this.currentQuestionNumber + "AR", this.currentQuestionNumber + "CR"];
+    },
+    listener: function() {
+        var o = this;
+        document.addEventListener('change', function(e) {
+            var currentId = o.currentQuestionNumber + e.target.value;
+            if (e.target.checked) {
+                o.optionCounter += 1;
+                if (o.optionCounter > 2){
+                    var mostRecent = o.currentlySelected.pop();
+                    o.currentlySelected = _.without(o.currentlySelected,mostRecent);
+                    o.currentlyNotSelected.push(mostRecent);
+                    o.optionCounter -= 1;
+                }
+                // console.log("option counter: " + o.optionCounter)
+                o.currentlySelected.push(currentId)
+                o.currentlyNotSelected = _.without(o.currentlyNotSelected,currentId)
+                o.answerSet.push(e.target.value);
+                // console.log("currently selected: "+o.currentlySelected);
+                // console.log("currently not selected: "+o.currentlyNotSelected);
+                if (o.optionCounter >= 2){
+                    document.getElementById('next').disabled = false;
+                    for(var unchecked in o.currentlyNotSelected){
+                        document.querySelector('label[for="'+ o.currentlyNotSelected[unchecked]+'"]').className = "checkbox-label-disabled";
+                        document.getElementById(o.currentlyNotSelected[unchecked]).checked = false;
+                    }
+                } else if (o.optionCounter < 2) {
+                    document.getElementById('next').disabled = true;
+                }
+                e.target.parentNode.className = "option-selected";
+            } else {
+                o.optionCounter -= 1;
+                if (o.optionCounter < 2){
+                    for (var unchecked in o.currentlyNotSelected){
+                        document.querySelector('label[for="'+ o.currentlyNotSelected[unchecked]+'"]').className = "";
+                    }
+                }
+                // console.log("option counter: " + o.optionCounter);               
+                // if (o.optionCounter === 2) {
+                //     document.getElementById('next').disabled = false;
+                // } else if (o.optionCounter !== 2) {
+                //     document.getElementById('next').disabled = true;
+                // }
+                o.currentlyNotSelected.push(currentId);
+                o.currentlySelected = _.without(o.currentlySelected, currentId);
+                e.target.parentNode.className = "option-not-selected";
+            }
+        });
+        document.getElementById('next').addEventListener('click', function() {
+            if (o.currentQuestionNumber === Think.questions.length - 1) {
+                o.addAnswersToArray(o.answerSet);
+                o.answerSet = []
+                o.showResults(o.answersArray);
+            } else if (o.optionCounter === 2) {
+                o.updateQuestionNumber("forward");
+                o.showCurrentQuestion();
+                document.getElementById('back').disabled = false;
+                document.getElementById('next').disabled = true;
+                o.addAnswersToArray(o.answerSet);
+                o.answerSet = []
+            }
+            if (o.currentQuestionNumber > 1){
+                console.log(Think.questions.length - o.currentQuestionNumber + " questions left");
+            }
+            o.reset();
+        });
+        document.getElementById('back').addEventListener('click', function() {
+            o.updateQuestionNumber("back");
+            o.answersArray = o.answersArray.slice(0,o.currentQuestionNumber);
+            o.showCurrentQuestion();
+            if (o.currentQuestionNumber === 0) {
+                document.getElementById('back').disabled = true;
+            }
+            o.reset();
+        });
+    },
+    showResults: function(resultsArray) {
+        var o = this;
+        console.log(resultsArray);
+        var letters = ["CS", "AS", "AR", "CR"];
+        var flatArray = [].concat.apply([], resultsArray);
+        console.log(flatArray);
+        for (var letter in letters) {
+            o.sumArrayTypes(letters[letter], flatArray);
+            o.totals[letters[letter]] = o.count;
+            o.count = 0;
+        }
+        console.log(this.totals);
+    },
+    addAnswersToArray: function(answerSet) {
+        this.answersArray.push(answerSet);
+    },
+    sumArrayTypes: function(x, array) {
+        var o = this;
+        for (var answer in array) {
+            if (array[answer] === x) {
+                o.count++;
+            }
+        }//,
     // count: function(number) { //limit checkbox selections to 2
     //   var currentCount = number + 1;
     //   if (currentCount === 2) { //show next button when two is reached
@@ -33,15 +146,15 @@ window.Think = _.extend(window.Think, {
     //     alert("only select two words");
     //   }
     // },
-    transition: function() {
-        alert();
-        // fading or sliding transition instead of alert
-    },
-    checkbox: function(characteristicsArray, valuesArray, questionNumber) { //print checkboxes
-        for (var i = 0; i <= 3; i++) {
-            document.write('<div id="' + questionNumber + '"><input type="checkbox" name="' + questionNumber + '.' + (i + 1) + '" value="' + valuesArray[i] + '" onclick="count(1)"/>' + characteristicsArray[i] + '</div><br/>'); //count is not defined?
-        }
-    },
+    // transition: function() {
+    //     alert();
+    //     // fading or sliding transition instead of alert
+    // },
+    // checkbox: function(characteristicsArray, valuesArray, questionNumber) { //print checkboxes
+    //     for (var i = 0; i <= 3; i++) {
+    //         document.write('<div id="' + questionNumber + '"><input type="checkbox" name="' + questionNumber + '.' + (i + 1) + '" value="' + valuesArray[i] + '" onclick="count(1)"/>' + '<label for="">' + characteristicsArray[i] + '</label></div><br/>'); //count is not defined?
+    //     }
+    // },
     // form: function(characteristicsArray, valuesArray) { //print form
     //     var questionNumber = 0;
     //     questionNumber++;
@@ -68,81 +181,6 @@ window.Think = _.extend(window.Think, {
     //         document.write('<button id="back">back</button>');
     //         document.write(nextButton);
     //     }
-    // },
-    updateQuestionNumber: function(direction) {
-        if(direction === "forward"){
-            this.currentQuestionNumber += 1;
-        } else if (direction === "back"){
-            this.currentQuestionNumber -= 1;
-        }
-    },
-    listener: function() {
-        var thinkObject = this;
-        document.addEventListener('change',function(e){
-          if(e.target.checked){
-            thinkObject.optionCounter += 1;
-            if (thinkObject.optionCounter === 2){
-              document.getElementById('next').disabled = false;
-            } else if (thinkObject.optionCounter !== 2){
-              document.getElementById('next').disabled = true;
-            }
-            thinkObject.answerSet.push(e.target.value);
-          } else {
-            thinkObject.optionCounter -= 1;
-            if (thinkObject.optionCounter === 2){
-              document.getElementById('next').disabled = false;
-            } else if (thinkObject.optionCounter !== 2){
-              document.getElementById('next').disabled = true;
-            }
-            thinkObject.answerSet = _.without(thinkObject.answerSet, e.target.value);
-          }
-        });
-        document.getElementById('next').addEventListener('click', function() {
-            if (thinkObject.currentQuestionNumber === Think.questions.length-1){
-              thinkObject.addAnswersToArray(thinkObject.answerSet);
-              thinkObject.answerSet = []
-              thinkObject.showResults(thinkObject.answersArray);
-            } else if (thinkObject.optionCounter === 2){
-              thinkObject.updateQuestionNumber("forward");
-              thinkObject.showCurrentQuestion();
-              thinkObject.optionCounter = 0;
-              document.getElementById('back').disabled = false;
-              document.getElementById('next').disabled = true;
-              thinkObject.addAnswersToArray(thinkObject.answerSet);
-              thinkObject.answerSet = []
-            }
-          console.log(Think.questions.length - thinkObject.currentQuestionNumber + " questions left");
-        }); 
-        document.getElementById('back').addEventListener('click', function() {
-            thinkObject.updateQuestionNumber("back");
-            thinkObject.showCurrentQuestion();
-            thinkObject.optionCounter = 0;
-            if (thinkObject.currentQuestionNumber === 0){
-              document.getElementById('back').disabled = true;
-            }
-        });   
-    },
-    showResults: function(){
-      var thinkObject = this;
-      var letters = ["CS","AS","AR","CR"];
-      var flatArray = [].concat.apply([],this.answersArray);
-      for (var letter in letters){
-          thinkObject.sumArrayTypes(letters[letter],flatArray);
-          thinkObject.totals[letters[letter]] = thinkObject.count;
-          thinkObject.count = 0;
-      }
-      console.log(this.totals);
-    },
-    addAnswersToArray: function(answerSet){
-      this.answersArray.push(answerSet);
-    },
-    sumArrayTypes: function(x,array) {
-      var thinkObject = this;
-      for (var answer in array){
-        if(array[answer] === x){
-          thinkObject.count++;
-        }
-      }
+    // }
     }
 });
-
